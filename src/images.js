77ae19de454b1d68879bdc250bbd2eda07d9a55e
GYEONGWON GIM <gyeongwon7986@gmail.com>
2022-07-21 14:19:27 +0900

    first commit

---
+++ src/images.js
@@ -0,0 +1,12 @@
+// FIXME:화면사이즈에 알맞은 크기의 이미지를 자동으로 불러오도록 만들기
+import CheckBoxOutline from '../assets/check_box_outline_blank-white-android/res/drawable-hdpi/baseline_check_box_outline_blank_white_18.png';
+import CheckBox from '..assets/check_box-white-android/res/drawable-hdpi/outline_check_box_white_18.png';
+import DeleteForever from '../assets/delete_forever-white-android/res/drawable-hdpi/baseline_delete_forever_white_18.png';
+import Edit from '../assets/edit-white-android/res/drawable-hdpi/baseline_edit_white_18.png'
+
+export const images = {
+    uncompleted: CheckBoxOutline,
+    completed: CheckBox,
+    delete: DeleteForever,
+    update:Edit,
+}
