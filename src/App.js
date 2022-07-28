84b25abc9648636cd78a98cbad4674322e7a360a
GYEONGWON GIM <gyeongwon7986@gmail.com>
2022-07-28 19:49:48 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React,  {useState} from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Dimensions } from 'react-native';
 import styled, {ThemeProvider} from 'styled-components/native';
 import Input from './components/Input';
 import {theme} from './theme';
-import {images} from './images';
-import IconButton from './components/IconButton';
 import Task from './components/Task';
-import { Dimensions } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -31,13 +29,34 @@ const List = styled.ScrollView`
 export default function App() {
   const width = Dimensions.get('window').width;
 
+  // const [isReady, setIsReady] = useState(false);
   const [newTask, setNewTask] = useState('');
-  const [tasks, setTasks] = useState({
-    '1': {id: '1', text: 'Hanbit', completed: false},
-    '2': {id: '2', text: 'React Native', completed: true},
-    '3': {id: '3', text: 'React Native Sample', completed: false},
-    '4': {id: '4', text: 'Edit TODO Item', completed: false}
-  });
+  const [tasks, setTasks] = useState({});
+
+  // useEffect(() => {
+  //   async function go() {
+  //     await _loadTasks();
+  //     setTimeout(() => {
+  //       setIsReady(true);
+  //     }, 2000);
+  //   }
+  //   go();
+  //   return () => {};
+  // }, []);
+
+  const _saveTasks = async tasks => {
+    try{
+      await AsyncStorage.setItem('tasks', JSON.stringfy(tasks));
+      setTasks(tasks);
+    } catch(e) {
+      console.error(e);
+    }
+  };
+
+  const _loadTasks = async() =>{
+    const loadedTasks = await AsyncStorage.getItem('tasks');
+    setTasks(JSON.parse(loadedTasks || '{}'));
+  };
 
   const _addTask = () => {
     const ID =Date.now().toString();
@@ -45,18 +64,35 @@ export default function App() {
       [ID]: {id: ID, text: newTask, completed: false },
     };
     setNewTask('');
-    setTasks({...tasks, ...newTaskObject });
+    _saveTasks({...tasks, ...newTaskObject });
   };
 
   const _deleteTask = id => {
     const currentTasks = Object.assign({}, tasks);
     delete currentTasks[id];
-    setTasks(currentTasks);
+    _saveTasks(currentTasks);
+    
+  };
+
+  const _toggleTask = id => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[id]['completed'] = !currentTasks[id]['completed'];
+    _saveTasks(currentTasks);
+  };
+  
+  const _updateTask = item => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[item.id] = item;
+    _saveTasks(currentTasks);
   };
 
   const _handleTextChange = text => {
     setNewTask(text);
   };
+
+  const _onBlur = () => {
+    setNewTask('');
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -71,12 +107,18 @@ export default function App() {
         value={newTask}
         onChangeText={_handleTextChange}
         onSubmitEditing={_addTask} 
+        onBlur={_onBlur}
         />
         <List width={width}>
           {Object.values(tasks)
           .reverse()
           .map(item => (
-            <Task key={item.id} item={item} deleteTask={_deleteTask} />
+            <Task key={item.id} 
+                  item={item} 
+                  deleteTask={_deleteTask}
+                  toggleTask={_toggleTask}
+                  updateTask={_updateTask} 
+                />
           ))}
         </List>
       </Container>
--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -5,19 +5,20 @@ import PropTypes from 'prop-types';
 import {images} from '../images';
 
 const Icon = styled.Image`
+    background-color: ${({theme, completed}) => 
+    completed ? theme.done : theme.text};
     width: 30px;
     height: 30px;
     margin: 10px;
 `;
 
-const IconButton = ({type, onPressOut, id }) => {
+const IconButton = ({type, onPressOut, id, completed }) => {
    const _onPressOut = () => {
     onPressOut(id);
    };
-   
     return (
         <TouchableOpacity onPressOut={_onPressOut}>
-            <Icon style={{tintColor: "white"}} source={type} />
+            <Icon source={type} completed={completed} />
         </TouchableOpacity>
     );
 }; 
@@ -30,6 +31,7 @@ IconButton.propTypes = {
     type: PropTypes.oneOf(Object.values(images)).isRequired,
     onPressOut: PropTypes.func,
     id: PropTypes.string,
+    completed: PropTypes.bool,
 };
 
 export default IconButton;
